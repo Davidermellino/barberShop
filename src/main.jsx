68a594b96ffa8decf4e2911fe438002a547c013f
Davide Ermellino <ermel@MacBook-Air.station>
2024-02-20 15:21:18 +0100

    cambiato percorso anche del font

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import "./fonts/Barlow-ExtraBold/barlow/Barlow-Bold.ttf"
+import "/fonts/Barlow-ExtraBold/barlow/Barlow-Bold.ttf"
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
